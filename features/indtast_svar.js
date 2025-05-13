cc1d8cde7c896a5ad4e23a460fd2f4447dba357b
Idasaabyfeld <ida-@hotmail.dk>
2025-05-13 22:01:06 +0200

    Update af feature
    Fået kryds billedet til at lukke overlayet

--- features/indtast_svar.js
+++ features/indtast_svar.js
@@ -5,13 +5,24 @@ document.addEventListener("DOMContentLoaded", () => {
     
   // Find knappen, som brugeren klikker på for at vise overlayet
     const indtastKnap = document.getElementById("indtast_knap");
-
-  // Find selve overlay-sektionen med billedet
+  
+    // Find selve overlay-sektionen med billedet
     const overlayBillede = document.getElementById("overlay_billede");
+ 
+    // Finder luk-knappen
+ const lukKnap = document.getElementById("luk_knap");
   
     // Når brugeren klikker på knappen, fjernes 'hidden'-attributten
     // Så vises overlayet på siden
     indtastKnap.addEventListener("click", () => {
       overlayBillede.removeAttribute("hidden");
     });
+ 
+// Skjul overlay når man klikker på luk-knappen
+lukKnap.addEventListener("click", () => {
+    overlayBillede.setAttribute("hidden", true);
   });
+});
+
+
+  
