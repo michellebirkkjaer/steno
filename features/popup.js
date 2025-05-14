84da062a19f97655fb17794eb1f8b286d447ac3c
sofievestermark <sofvestermark@gmail.com>
2025-05-14 11:29:13 +0200

    knapper linket

--- features/popup.js
+++ features/popup.js
@@ -2,27 +2,6 @@
 
 
 
-// const guessButton = document.querySelector('button img.gaet').parentElement;
-// const popup = document.getElementById('popup');
-// const submitGuess = document.getElementById('submitGuess');
-
-// const correctName = "Villads"; //
-
-// // Vis popup når "gæt her"-knappen klikkes
-// guessButton.addEventListener('click', () => {
-//   popup.classList.remove('hidden');
-// });
-
-// // Håndtér gæt
-// submitGuess.addEventListener('click', () => {
-//   const userGuess = document.getElementById('guessInput').value.trim().toLowerCase();
-//   if (userGuess === correctName.toLowerCase()) {
-//     window.location.href = 'maven_korrekt.html'; // 
-//   } else {
-//     window.location.href = 'maven_forkert.html'; // 
-//   }
-// });
-
 // Vent med at køre JavaScript, indtil hele HTML'en er indlæst
 document.addEventListener("DOMContentLoaded", () => {
     
@@ -64,22 +43,5 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = 'maven_forkert.html';
     }
   });
-//   okKnap.addEventListener("click", () => {
-//   const brugerSvar = svarInput.value.trim();
-//   const korrektSvar = "Villads";
-  
-//   if (brugerSvar === korrektSvar) {
-//       // Hvis korrekt, send brugeren videre til ny side
-//       window.location.href = "maven_korrekt.html";
-//   } else {
-//       // Hvis forkert, vis forkert-overlay
-//       window.location.href = 'maven_forkert.html';
-//   }
-//   });
-  
-  // "Prøv igen" lukker forkert-overlay
-//   provIgenKnap.addEventListener("click", () => {
-//   forkertOverlay.setAttribute("hidden", true);
-//   svarInput.value = "";
-//   });
+
   });
