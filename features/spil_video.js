0783b82fc04754af31841b0a3c47d8c70e754265
michellebirkkjaer <michelle.birkkjaer@hotmail.dk>
2025-05-16 11:17:38 +0200

    noter

--- features/spil_video.js
+++ features/spil_video.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const overlay = document.getElementById('overlay');
-const video = document.getElementById('myVideo');
+const video = document.getElementById('myVideo'); //Henter <video>-elementet med id myVideo og gemmer det i variablen video.
 
-overlay.addEventListener('click', () => {
-  overlay.style.display = 'none';
-  video.muted = false;
-  video.currentTime = 0;
-  video.play();
+overlay.addEventListener('click', () => { //Sætter en klik-handler på overlayet
+  overlay.style.display = 'none'; //Skjuler overlayet ved at sætte display: none;
+  video.muted = false; //Slår lyden til på videoen (hvis den tidligere var muted – fx pga. autoplay-begrænsninger).
+  video.currentTime = 0; //Spoler videoen tilbage til start (0 sekunder).
+  video.play(); //Starter videoafspilningen.
 });
