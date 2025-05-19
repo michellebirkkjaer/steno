ee9d912d7430ef41a93b4981c7552f2eab3fb8c8
MalouStoustrup <malou.stoustrup@gmail.com>
2025-05-19 09:58:11 +0200

    vend papir lyd

---
+++ features/vend_side_lyd.js
@@ -0,0 +1,16 @@
+const lyd = document.getElementById("klik_lyd");
+
+document.querySelectorAll(".pil_knap").forEach(link => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault(); // Stop linket i at skifte side med det samme
+    const destination = link.getAttribute("href"); // Gem linkets destination
+
+    lyd.currentTime = 0;
+    lyd.play();
+
+    // Vent lidt, før vi skifter side (f.eks. 900 ms)
+    setTimeout(() => {
+      window.location.href = destination;
+    }, 800); // Tilpas tiden efter hvor lang lyden er
+  });
+});
